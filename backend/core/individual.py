18e9ea32f0a6644bcc81744a6854a765459a7589
Ravi Sinha <ravisinha@my.unt.edu>
2026-04-10 18:37:56 -0500

    Update Individual with typing and better structure

--- backend/core/individual.py
+++ backend/core/individual.py
@@ -1,10 +1,17 @@
-from dataclasses import dataclass
 import numpy as np
+from typing import Callable
 
 
-@dataclass
 class Individual:
-    '''A genome is always a Numpy vector.
-    The framework is dimension-agnostic'''
-    genome: np.ndarray
-    fitness: float | None = None
+    '''Represents one candidate organism
+    in the population'''
+
+    def __init__(self, genome: np.ndarray):
+        self.genome = genome
+        self.fitness = None
+
+    def evaluate(self, fitness_function: Callable):
+        if self.fitness is None:
+            self.fitness = fitness_function(self.genome)
+
+        return self.fitness
